9acf2d1158ec85cf93be9789e7f6303198d09fd9
bendevera <ben10devera@gmail.com>
2020-07-09 08:43:18 -0600

    saving completed assignment

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,5 @@
 # Your code here
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -8,13 +9,6 @@ def slowfun_too_slow(x, y):
     v %= 982451653
 
     return v
-
-def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
 
 
 
